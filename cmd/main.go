8bfffee9d5a6c2543c72cc54e3fc62e07a0f0428
ybgr111 <ybgr111@mail.ru>
2024-01-03 12:36:46 +0300

    Начальная структурка

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	fmt.Println("hellow golang")
+	fmt.Println(uuid.New())
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ybgr111/ne
+
+go 1.21.5
+
+require github.com/google/uuid v1.5.0 // indirect
