a9c2bd0296ad46e5f6ebaf4d4e244682f1203b6c
ybgr111 <ybgr111@mail.ru>
2024-01-03 17:11:56 +0300

    Укладка бетона

---
+++ cmd/calendar/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var configFile string
+
+func init() {
+	flag.StringVar(&configFile, "config", "/etc/calendar/config.toml", "Path to configuration file")
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.Arg(0) == "version" {
+		printVersion()
+		return
+	}
+
+	config := NewConfig()
+	logg := logger.New(config.Logger.Level)
+
+	fmt.Println("hellow golang")
+}
